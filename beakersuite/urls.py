d2b459aae00183762ba1ef44c50906a3189b12ba
Richard Marko <rmarko@redhat.com>
2011-12-05 21:56:30 +0100

    Rewrite views to support detail and block list view
     - better view logic
     - better url sample
     - fixed block list

--- beakersuite/urls.py
+++ beakersuite/urls.py
@@ -1,19 +1,20 @@
-from django.conf import settings
 from django.conf.urls.defaults import patterns, url
 
-from views import list_results
+from views import list_results, list_single
 
 urlpatterns = patterns('',
     url(r'latest/$',
         list_results,
-        {'data_path': settings.BEAKERSUITE_DATA_PATH,
-        'num_latest': 2,
+        {'num_latest': 2,
         'template_name':'beakersuite/list.html'},
         name='beakersuite_latest'),
 
     url(r'list/$',
         list_results,
-        {'data_path': settings.BEAKERSUITE_DATA_PATH,
-        'template_name':'beakersuite/block_list.html'},
+        {'template_name':'beakersuite/block_list.html'},
         name='beakersuite_list'),
+
+    url(r'(?P<dirname>.+)/$',
+        list_single,
+        name='beakersuite_single'),
     )
--- beakersuite/views.py
+++ beakersuite/views.py
@@ -1,26 +1,54 @@
 import os
 
+from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+
+default_datapath = settings.BEAKERSUITE_DATA_PATH
 
 def render_error(request, msg):
     return render_to_response('beakersuite/error.html',
         dict(error=msg),
         RequestContext(request, {}))
 
-def list_results(request, data_path,
+def list_results(request,
+    data_path=default_datapath,
     num_latest=None,
     template_name='beakersuite/list.html'):
 
     if not os.path.isdir(data_path):
         render_error(request, 'No such dir: %s' % data_path)
 
+    rundata = list_dirs(data_path, None, num_latest)
+    return render_to_response(template_name,
+        dict(runs=rundata, data_path=data_path),
+        RequestContext(request, {}))
+
+def list_single(request,
+    dirname,
+    data_path=default_datapath,
+    template_name='beakersuite/list.html'):
+
+    if not os.path.isdir(os.path.join(data_path, dirname)):
+        render_error(request, 'No such dir: %s' %
+            os.path.join(data_path, dirname))
+
+    rundata = list_dirs(data_path, [dirname], None)
+    return render_to_response(template_name,
+        dict(runs=rundata, data_path=data_path),
+        RequestContext(request, {}))
+
+
+def list_dirs(data_path, only_dirs, num_latest):
     test_count = 0
     rundata = []
     dirs = os.listdir(data_path)
     dirs.sort(reverse=True)
     if num_latest:
         dirs = dirs[0:num_latest]
+
+    if only_dirs:
+        dirs = filter(lambda x: x in only_dirs, dirs)
 
     for run in dirs:
         fname = os.path.join(data_path, run, 'results')
@@ -53,7 +81,4 @@ def list_results(request, data_path,
         rundata.append(dict(name=run, result=overal_fail, tests=tests))
         rundata.sort(reverse=True)
 
-    return render_to_response(template_name,
-        dict(runs=rundata, test_count=range(1, test_count+1),
-             data_path=data_path),
-        RequestContext(request, {}))
+    return rundata
