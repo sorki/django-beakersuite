53a41e99a4382a7a46a93b967acadc65aa5f1c47
Richard Marko <rmarko@redhat.com>
2011-12-05 20:51:22 +0100

    Provide better urls

--- beakersuite/urls.py
+++ beakersuite/urls.py
@@ -4,8 +4,16 @@ from django.conf.urls.defaults import patterns, url
 from views import list_results
 
 urlpatterns = patterns('',
+    url(r'latest/$',
+        list_results,
+        {'data_path': settings.BEAKERSUITE_DATA_PATH,
+        'num_latest': 2,
+        'template_name':'beakersuite/list.html'},
+        name='beakersuite_latest'),
+
     url(r'list/$',
         list_results,
-        {'data_path': settings.BEAKERSUITE_DATA_PATH},
+        {'data_path': settings.BEAKERSUITE_DATA_PATH,
+        'template_name':'beakersuite/block_list.html'},
         name='beakersuite_list'),
     )
